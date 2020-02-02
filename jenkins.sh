a345a50d47f2e8555a6f837209a2b7e7c85b5566
Zeeshan <zeeshantariq.me@gmail.com>
2020-02-02 23:40:54 +0500

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -5,5 +5,9 @@ git reset --hard;\
         git fetch --all;\
         git pull origin master;\
 git pull;\
+git checkout -b jen-dev;\
+git checkout master;\
+git branch -D jen-dev;\
+git merge master;\
 
 echo "pulled"
